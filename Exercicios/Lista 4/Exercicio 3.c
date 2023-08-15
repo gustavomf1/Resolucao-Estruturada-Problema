78de88ab46f19c80aec710cf84688198e19080e6
Gustavo M. França <gustavo_mf1@hotmail.com>
2023-08-15 08:08:49 -0300

    att 15/08

--- Exercicios/Lista 4/Exercicio 3.c
+++ Exercicios/Lista 4/Exercicio 3.c
@@ -16,14 +16,58 @@ int calcular_idade(){
 	
 	aa = 2023;
 	idade = aa - an;
-	return idade;
+	printf("\nEsta eh sua idade: %d", idade);
 }
 
+int calcular_media(){
+	int n1, n2, n3, n4;
+	float media;
+	system("cls");
+	printf("Digite a nota 1: ");
+	scanf("%d", &n1);
+	
+	printf("Digite a nota 2: ");
+	scanf("%d", &n2);
+	
+	printf("Digite a nota 3: ");
+	scanf("%d", &n3);
+	
+	printf("Digite a nota 4: ");
+	scanf("%d", &n4);
+	
+	media = (n1 + n2 + n3+ n4) / 4;
+	printf("Media: %f", media);
+}
+
+int mult_numeros(){
+	int n1, n2, mult;
+	system("cls");
+	printf("Digite o n1: ");
+	scanf("%d", &n1);
+	
+	printf("Digite o n2: ");
+	scanf("%d", &n2);
+	
+	mult = n1 * n2;
+	printf("Resultado da multiplicacao: %d", mult);
+}
+
+int div_numeros(){
+	int n1, n2;
+	float div;
+	printf("Digite o n1: ");
+	scanf("%d", n1);
+	
+	printf("Digite o n2: ");
+	scanf("%d", n2);
+	
+	div = n1 / n2;
+	printf("Resultado da divisao: %f", div);
+}
 int menu(){
-	int opcao, retorno;
+	int opcao;
 	do{
-		system("cls");
-		printf("Informe a Opcao: ");
+		printf("\nInforme a Opcao: ");
 		printf("\n1 - Calcular Idade: ");
 		printf("\n2 - Media de 3 Notas: ");
 		printf("\n3 - Maior Numero: ");
@@ -34,16 +78,23 @@ int menu(){
 		
 		if(opcao == 1){
 			calcular_idade();
-			retorno = calcular_idade();
 		}
 		else
 			if(opcao == 2){
-				
+				calcular_media();		
 			}
+			else
+				if(opcao == 3){
+					mult_numeros();	
+				}
+				else
+					if(opcao == 4){
+						div_numeros();	
+					}
 	}while(opcao != 5);
 }
 
 void main(){
+	int retorno;
 	menu();	
-	
 }
